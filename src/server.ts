8616eb7ede94a320c30baaa8ebd400f2cdeecb64
MatheusT <matheusgarciajbs@gmail.com>
2023-09-22 15:25:23 -0300

    db work

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,25 @@
 import express, { request, response }  from "express";
+import { pool } from "./mysql";
+import { v4 as uuidv4 } from "uuid";
 
 const app = express()
 
 app.use(express.json())
 
-app.get('/', (request, response) => {
-    const {name, email, password } = request.body
+app.post('/user', (request, response) => {
+    const { name, email, password } = request.body
+    pool.getConnection((err: any, connection: any) => {
+        connection.query(
+            'INSERT INTO users (user_id, name, email, password) VALUES (?,?,?,?)',
+            [uuidv4(),name, email, password],
+            (error:any, result:any, fields:any) => {
+                if (error) {
+                    return response.status(400).json(error)
+                }
+                response.status(200).json({success: true})
+            }
+        )
+    })
 })
 
 app.listen(4000)
