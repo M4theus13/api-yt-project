d5cadfc366dddca88628b3759980cb7557679d7c
Matheus Garcia <matheusgarciajbs@gmail.com>
2023-09-22 21:00:37 -0300

    criptografia de senha, token de autenticacao

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,75 @@
 import express, { request, response }  from "express";
 import { pool } from "./mysql";
 import { v4 as uuidv4 } from "uuid";
+import { hash, compare } from 'bcrypt'
+import { sign } from 'jsonwebtoken'
 
 const app = express()
 
 app.use(express.json())
 
-app.post('/user', (request, response) => {
+//cadastro
+app.post('/user/sign-up', (request, response) => {
     const { name, email, password } = request.body
+
+    hash(password, 10, (error, hash) => {
+        if (error) {
+            return response.status(500).json(error)
+        }
+
+        pool.getConnection((err: any, connection: any) => {
+            connection.query(
+                'INSERT INTO users (user_id, name, email, password) VALUES (?,?,?,?)',
+                [uuidv4(),name, email, hash],
+                (error:any, result:any, fields:any) => {
+                    connection.release()
+                    if (error) {
+                        return response.status(400).json(error)
+                    }
+                    response.status(200).json({success: true})
+                }
+            )
+        })
+
+    })
+})
+
+//login
+app.post('/user/sign-in', (request, response) => { 
+    const { email, password } = request.body
+
     pool.getConnection((err: any, connection: any) => {
         connection.query(
-            'INSERT INTO users (user_id, name, email, password) VALUES (?,?,?,?)',
-            [uuidv4(),name, email, password],
-            (error:any, result:any, fields:any) => {
+            'SELECT * FROM users WHERE email = ?',
+            [email],
+            (error:any, results:any, fields:any) => {
+                connection.release()
                 if (error) {
-                    return response.status(400).json(error)
+                    return response.status(400).json({error: "Erro na sua autenticação"})
                 }
-                response.status(200).json({success: true})
+
+                compare(password, results[0].password, (err, result) => {
+                    if (err) {
+                        return response.status(400).json({error: "Erro na sua autenticação"})
+                    }
+
+                    if (results) {
+                        //JWT JSON WEB TOKEN
+                        const token = sign({
+                            id: results[0].user_id,
+                            email: results[0].email
+                        }, "segredo", {expiresIn:"1d"})
+                        
+                        console.log(token)
+
+                        return response.status(200).json({token: token})
+
+                    }
+                })
             }
         )
     })
 })
+
 
 app.listen(4000)
